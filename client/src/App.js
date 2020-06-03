1b3a617562957c510a5aa934253b8ca2c4fae14f
olavokruel <olavokruel@gmail.com>
2020-06-02 23:53:13 -0300

    connectivity working between client and api

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
-
+import axios from 'axios';
 
 const App = props => {
+
+  const [test, setTest] = useState('');
+
+  const api = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    axios.get(`${api}/test`)
+      .then(res => setTest(res.data))
+  }, [api])
+  
   return (
     <div className="App">
+      {test}
     </div>
   );
 }
